9c02c0fe702330b049f83d81b8bf2fe03c2798f8
lesliehealray <lesliehealray@gmail.com>
2019-10-28 19:06:51 -0400

    - adjusted models so that snippet is self referencing thus able to be copied and the derivatives tracked. added djangorestframework, AUTH_USER_MODEL to setting

--- snipp_dogg/admin.py
+++ snipp_dogg/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from snipp_dogg.models import User, Language, CodeSnippet
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Language)
+admin.site.register(CodeSnippet)
--- snipp_dogg/models.py
+++ snipp_dogg/models.py
@@ -7,10 +7,35 @@ class User(AbstractUser):
     password = models.CharField(max_length=255)
     profile_description = models.TextField()
 
-class Created_Snips:
-    snip = models.TextField()
-    user = models.IntegerField()
+    def __str__(self):
+        return self.email
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
+    extension = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.name} ({self.extension})"
+
+class CodeSnippet(models.Model):
     description = models.CharField(max_length=255)
-    language = models.CharField(max_length=255)
-    time_created = models.DateTimeField(default=timezone.now)
-    time_updated = models.DateTimeField(default=timezone.now)
+    body = models.TextField()
+    language = models.ForeignKey(
+        to=Language, 
+        on_delete=models.SET_NULL,
+        related_name='language',
+        null=True, blank=True)
+    user = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE,
+        related_name='user')
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
+    source = models.ForeignKey(
+        'self', 
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='derivatives')
+
+    def __str__(self):
+        return self.description
