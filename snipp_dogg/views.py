ba2ddc6c07c8eb7054584d5ce32b6ae9359d5bf3
JBHutson <hutsonjb@gmail.com>
2019-10-30 12:29:26 -0400

    url change

--- snipp_dogg/views.py
+++ snipp_dogg/views.py
@@ -8,7 +8,6 @@ def register_user(request):
         form = SnippDogdUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Your Account has Been Created! You May Now Login!')
             return redirect('login')
     else:
