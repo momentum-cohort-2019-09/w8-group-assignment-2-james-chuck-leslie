ff5b369dedcfb72cc6afbca9fc887dbf86f131dc
JBHutson <hutsonjb@gmail.com>
2019-10-30 19:45:02 -0400

    actual editSnipp

--- snipp_dogg/views.py
+++ snipp_dogg/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from snipp_dogg.models import CodeSnippet
 from snipp_dogg.forms import SnippDogdUserCreationForm, CreateSnippForm
 
 def register_user(request):
@@ -25,6 +26,23 @@ def create_snipp(request):
             snipp.save()
             messages.success(request, f'Your Snipp has Been Created!')
             return redirect('profile')
+    else:
+        form = CreateSnippForm()
+    return render(request, "snipp_dogg/create.html",{
+        "form": form
+    })
+
+def edit_snipp(request, pk):
+    og_snipp = get_object_or_404(CodeSnippet, id=pk)
+    if request.method == 'POST':
+        form = CreateSnippForm(request.POST)
+        if form.is_valid():
+            snipp = form.save(commit=False)
+            snipp.source = og_snipp
+            snipp.user = request.user
+            snipp.save()
+            messages.success(request, f'Your Snipp has Been Edited!')
+            return redirect('homepage')
     else:
         form = CreateSnippForm()
     return render(request, "snipp_dogg/create.html",{
