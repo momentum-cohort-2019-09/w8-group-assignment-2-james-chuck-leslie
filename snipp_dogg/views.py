e231e4f74dd382a25b6a316c6f9666e86011f6ba
JBHutson <hutsonjb@gmail.com>
2019-10-30 18:49:11 -0400

    branch says snippedit, but actually createsnipp

--- snipp_dogg/views.py
+++ snipp_dogg/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from snipp_dogg.forms import SnippDogdUserCreationForm
+from snipp_dogg.forms import SnippDogdUserCreationForm, CreateSnippForm
 
 def register_user(request):
     if request.method == 'POST':
@@ -13,6 +13,21 @@ def register_user(request):
     else:
         form = SnippDogdUserCreationForm()
     return render(request, "snipp_dogg/register.html",{
+        "form": form
+    })
+
+def create_snipp(request):
+    if request.method == 'POST':
+        form = CreateSnippForm(request.POST)
+        if form.is_valid():
+            snipp = form.save(commit=False)
+            snipp.user = request.user
+            snipp.save()
+            messages.success(request, f'Your Snipp has Been Created!')
+            return redirect('profile')
+    else:
+        form = CreateSnippForm()
+    return render(request, "snipp_dogg/create.html",{
         "form": form
     })
 
