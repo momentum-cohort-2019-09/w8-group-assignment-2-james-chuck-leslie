21e7aed53868cdf31cf042d5184fb7de56cd5a9e
Charles Wines <ccrecorder@gmail.com>
2019-10-30 21:32:49 -0400

    makes react fully operational

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'snipp_dogg',
     'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 from snipp_dogg import views as snipp_views
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('api/snipp_dogg/', api.CodeSnippetListCreate.as_view()),
     path('api/snipp_dogg/user', api.UserListCreate.as_view()),
