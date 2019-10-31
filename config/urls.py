f132cc0d47c60fc9a089b2c09f6f212ebad36caf
Charles Wines <ccrecorder@gmail.com>
2019-10-30 21:54:49 -0400

    finally, react fully works!

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from snipp_dogg import api
 from django.contrib.auth import views as auth_views
 from snipp_dogg import views as snipp_views
