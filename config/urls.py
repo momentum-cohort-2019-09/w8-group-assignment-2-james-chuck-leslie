932e7b443174bf740ead3abec7c10e8442425cee
lesliehealray <lesliehealray@gmail.com>
2019-10-30 23:18:55 -0400

    -added snipp listing and snipp details views, urls, templates

--- config/urls.py
+++ config/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('snipp_dogg/profile', snipp_views.profile, name='profile'),
     path('snipp_dogg/register', snipp_views.register_user, name='register_user'),
     path('snipp_dogg/login', auth_views.LoginView.as_view(template_name='snipp_dogg/login.html'), name='login'),
-    path('snipp_dogg/logout', auth_views.LogoutView.as_view(template_name='snipp_dogg/logout.html'), name='logout')
+    path('snipp_dogg/logout', auth_views.LogoutView.as_view(template_name='snipp_dogg/logout.html'), name='logout'),
+    path('snipp_dogg/listing', snipp_views.snipp_list, name='snipp_list'),
+    path('snipp_dogg/display/<int:pk>', snipp_views.snipp_detail, name='snipp_detail')
 ]
