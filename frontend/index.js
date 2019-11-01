b20196faa39b138c112b479c0e527d7eb00e4801
JBHutson <hutsonjb@gmail.com>
2019-11-01 13:38:45 -0400

    allows 4 spaces for tab in textarea

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,16 @@
 import './prism.css';
 import './prism.js';
 import './index.scss';
+
+const codebox = document.getElementById('codebox')
+
+codebox.addEventListener("keydown", (e) => {
+    let { keyCode } = e;
+    let { value, selectionStart, selectionEnd } = codebox;
+
+    if (keyCode === 9) {  // TAB = 9
+      e.preventDefault();
+      codebox.value = value.slice(0, selectionStart) + "    " + value.slice(selectionEnd);
+      codebox.setSelectionRange(selectionStart+4, selectionStart+4)
+    }
+});
